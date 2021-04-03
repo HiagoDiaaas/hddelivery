8038c79d839631db2e5151e35fd73e2d114f5bdd
Hiago <hiagosmd@hotmail.com>
2021-04-03 19:53:15 -0300

    Projet created

---
+++ backend/src/main/java/com/hiagodias/hddelivery/HddeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.hiagodias.hddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HddeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HddeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hiagodias/hddelivery/HddeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiagodias.hddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HddeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
