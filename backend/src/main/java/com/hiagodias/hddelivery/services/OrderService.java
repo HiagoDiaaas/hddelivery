92f54e806ea3feb635a279aa879bc40311f2a5b9
Hiago <hiagosmd@hotmail.com>
2021-04-08 21:47:07 -0300

    POST /orders

--- backend/src/main/java/com/hiagodias/hddelivery/services/OrderService.java
+++ backend/src/main/java/com/hiagodias/hddelivery/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.hiagodias.hddelivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,19 +9,40 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.hiagodias.hddelivery.dto.OrderDTO;
+import com.hiagodias.hddelivery.dto.ProductDTO;
 import com.hiagodias.hddelivery.entities.Order;
+import com.hiagodias.hddelivery.entities.OrderStatus;
+import com.hiagodias.hddelivery.entities.Product;
 import com.hiagodias.hddelivery.repositories.OrderRepository;
+import com.hiagodias.hddelivery.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 		
 		
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+			
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 	
 }
